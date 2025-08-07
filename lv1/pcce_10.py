c81f22826db8507a0827201da877a1be8de19605
sunju1 <ciwrssn10@naver.com>
2025-08-07 17:49:09 +0900

    다른 사람 풀이... 이게 더 나음

--- lv1/pcce_10.py
+++ lv1/pcce_10.py
@@ -66,6 +66,34 @@ def solution(mats, park):
     return -1
 
 
+"""
+* 다른 사람의 풀이! 이게 더 좋은 것 같아 적어놓는다.
+
+def can_place_mat(park, size):
+    # 공원의 행(row)와 열(col) 길이
+    rows, cols = len(park), len(park[0])
+
+    # park에서 주어진 size의 돗자리를 놓을 수 있는지 확인
+    for i in range(rows - size + 1):
+        for j in range(cols - size + 1):
+            # size x size 크기의 공간이 모두 '-1'인지 확인
+            if all(park[x][y] == '-1' for x in range(i, i + size) for y in range(j, j + size)):
+                return True
+    return False
+
+def solution(mats, park):
+    # 돗자리 크기 내림차순으로 정렬
+    mats.sort(reverse=True)
+
+    # 각 돗자리 크기에 대해 놓을 수 있는지 확인
+    for size in mats:
+        if can_place_mat(park, size):
+            return size
+
+    return -1
+"""
+
+
 m = [5, 3, 2]
 p = [["A", "A", "-1", "B", "B", "B", "B", "-1"],
      ["A", "A", "-1", "B", "B", "B", "B", "-1"],
